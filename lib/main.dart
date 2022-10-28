3c92464a96d11ed87301d1b41c5d1849d79fe16e
Yasir Hassan <yasir.hassan1993@hotmail.com>
2022-10-28 12:10:22 +0500

    Added dark and light mode theme colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:reddit_clone/theme/pallete.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,10 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Reddit Clone',
+      theme: Pallete.darkModeTheme,
       home: const SizedBox(),
     );
   }
