d482eec175a79b423a5a5c5707950aa4c12a8773
Yasir Hassan <yasir.hassan1993@hotmail.com>
2022-10-28 21:28:07 +0500

    Storing data in Provider done.

--- lib/feature/auth/controller/auth_controller.dart
+++ lib/feature/auth/controller/auth_controller.dart
@@ -2,20 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/core/utils.dart';
 import 'package:reddit_clone/feature/auth/repository/auth_repository.dart';
+import 'package:reddit_clone/models/user_model.dart';
 
-final authControllerProvider = Provider(
+final userProvider = StateProvider<UserModel?>((ref) => null);
+
+final authControllerProvider = StateNotifierProvider<AuthController, bool>(
   (ref) => AuthController(
-    authRepository: ref.read(authRepositoryProvider),
+    authRepository: ref.watch(authRepositoryProvider),
+    ref: ref,
   ),
 );
 
-class AuthController {
+class AuthController extends StateNotifier<bool> {
   final AuthRepository _authRepository;
-  AuthController({required AuthRepository authRepository})
-      : _authRepository = authRepository;
+  final Ref _ref;
+  AuthController({required AuthRepository authRepository, required Ref ref})
+      : _authRepository = authRepository,
+        _ref = ref,
+        super(false);
 
   void signInWithGoogle(BuildContext context) async {
+    state = true;
     final user = await _authRepository.signInWithGoogle();
-    user.fold((l) => showSnackBar(context, l.message), (r) => null);
+    state = false;
+    user.fold(
+      (l) => showSnackBar(context, l.message),
+      (userModel) =>
+          _ref.read(userProvider.notifier).update((state) => userModel),
+    );
   }
 }
--- lib/feature/auth/screens/login_screen.dart
+++ lib/feature/auth/screens/login_screen.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/core/common/loader.dart';
 import 'package:reddit_clone/core/common/sign_in_button.dart';
 import 'package:reddit_clone/core/contants/constants.dart';
+import 'package:reddit_clone/feature/auth/controller/auth_controller.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends ConsumerWidget {
   const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isLoading = ref.watch(authControllerProvider);
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -27,31 +32,33 @@ class LoginScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(height: 22.5),
-            const Text(
-              'Dive into anything',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 0.5,
+      body: isLoading
+          ? const Loader()
+          : Center(
+              child: Column(
+                children: [
+                  const SizedBox(height: 22.5),
+                  const Text(
+                    'Dive into anything',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 0.5,
+                    ),
+                  ),
+                  const SizedBox(height: 22.5),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset(
+                      Constants.loginEmotePath,
+                      height: 300,
+                    ),
+                  ),
+                  const SizedBox(height: 17.5),
+                  const SignInButton(),
+                ],
               ),
             ),
-            const SizedBox(height: 22.5),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset(
-                Constants.loginEmotePath,
-                height: 300,
-              ),
-            ),
-            const SizedBox(height: 17.5),
-            const SignInButton(),
-          ],
-        ),
-      ),
     );
   }
 }
